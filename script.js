11b9a81e14fb8a65316cfa0589c8a9854a028eaf
leticiamarty <leticiah0897@hotmail.com>
2019-04-17 12:12:35 -0300

    fazer exercicio de for

---
+++ script.js
@@ -0,0 +1,111 @@
+// let filmes = ['barbie','moranguinho','sininho','scoob doo'];
+// console.log([0]);
+// console.log([1]);
+// console.log([2]);
+// console.log([3]);
+
+// console.log(filmes);
+
+// let filmes = ['barbie','moranguinho','sininho','scoob doo',
+// 'titanic','desenhos animados'];
+// let i ;
+
+// // farofa for of ele vai imprimir oque tem na lista
+
+// for(let item of  filmes;){
+//     console.log(item)
+//  farinha for in ele imprimi os indices "0.1.2.3.4..." numeros ou simbolos.
+
+
+
+// let indice;
+// for(i=0; i<5; i++){
+//     console.log=filmes[i])         //console.log aparece na barrinha de especionar//
+// }
+
+// function calculadora(a,b){
+//     return a+b;
+// }
+// console.log(calculadora (2,3));
+
+// fuction calculadora(){
+//     return "ola lucas!"
+// }
+// console.log(calculadora());
+
+
+// function calcmedia (nota1,nota2,nota3,nota4){ 
+//     return (nota1+nota2+nota3+nota4)/4;
+// }
+
+// console.log(calcmedia(2,9,7,9));
+//  let calcmedia = fuction(n1,n2,n3,n4){      
+
+//      return((n1,n2,n2,n3)/3).toFixed(1)
+//  }
+
+//  calcmedia =(a,b, blablabla,carro) =>{
+//     return((n1,n2,n3,n4)/3).toFixed(1
+//         )
+//  }
+
+// let filmes=[
+//     /* 0 */ {titulo:"moranguinho",classificao:14},
+//     /* 1 */ {titulo:"scoob doo",classificao:0},
+//     /* 2 */ {titulo:"ninja assasino",classificao:18},
+// ]
+
+// let idade= prompt("coloque sua idade.");
+//         console.log("lista de filmes que voce pode assistir.");
+//     for(let filme of filmes){
+//         if(idade >= filme.classificao){
+//             console.log(filme.titulo);
+//         }
+//     }                    
+
+let musicas=[
+    /* 0 */ {titulo:"sertaneijo",idade:14},    
+    /* 1 */ {titulo:"infantil",idade:0},
+    /* 2 */ {titulo:"funk",idade:21},
+]
+let idade= prompt("informe sua idade aqui !");
+        console.log("lista de musicas que voce pode escutar.");
+    for(let indice in musicas){
+        if(idade >= musicas[indice].idade){
+            console.log(musicas[indice].titulo);
+        }
+}
+
+let casa={
+    cozinha:["geladeira,fogao"]
+
+}
+
+casa.cozinha[indice]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
